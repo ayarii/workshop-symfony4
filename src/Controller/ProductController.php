9f2d7192d064bcf72a6d0f7cfa2528f9deffbfb5
ayarii <asma.ayari@esprit.tn>
2020-11-23 19:53:14 +0100

    firest commit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product", name="product")
+     */
+    public function index(): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+
+    /**
+     * @Route("/list", name="listProduct")
+     */
+    public function listProduct()
+    {
+        return new Response("La liste des produits");
+    }
+
+}
