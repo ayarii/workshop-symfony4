d919a93faa462ae207b6dfad134dec76ac9b567a
AmineMissaoui <amine.missaoui@dqlick.com>
2020-11-28 17:19:34 +0100

    seance 28/11 crud product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,7 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use App\Form\ProductType;
+
 
 class ProductController extends AbstractController
 {
@@ -23,7 +27,65 @@ class ProductController extends AbstractController
      */
     public function listProduct()
     {
-        return new Response("La liste des produits");
+        //return new Response("La liste des produits");
+        $products=$this->getDoctrine()->getRepository(Product::class)->findAll();
+        return $this->render("product/listProducts.html.twig",array('listProducts'=>$products));
+
     }
 
+    /**
+     * @Route("/removeProduct/{id}", name="removeProduct")
+     */
+
+    public function deleteProduct($id){
+        $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
+        $em=$this->getDoctrine()->getManager();
+        $em->remove($product);
+        $em->flush();
+        return
+         $this->redirectToRoute("listProduct");
+    }
+
+    /**
+     * @Route("/showProduct/{id}", name="showProduct")
+     */
+
+    public function showProduct($id){
+        $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
+        //var_dump($product).die();
+        return $this->render("product/showProduct.html.twig",array('product'=>$product));
+   }
+    /**
+     * @Route("/addProduct", name="addProduct")
+     */
+   public function addProduct(Request $request){
+       $product = new Product();
+       $formProduct = $this->createForm(ProductType::class,$product);
+       $formProduct->handleRequest($request);
+       if($formProduct->isSubmitted()){
+           $em=$this->getDoctrine()->getManager();
+           $em->persist($product);
+           $em->flush();
+           return $this->redirectToRoute("listProduct");
+       }
+
+       return $this->render("product/addProduct.html.twig",array('formProduct'=>$formProduct->createView()));
+   }
+
+    /**
+     * @Route("/updateProduct/{id}", name="updateProduct")
+     */
+    public function updateProduct(Request $request, $id){
+        $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
+        $formProduct = $this->createForm(ProductType::class,$product);
+
+        $formProduct->handleRequest($request);
+
+        if($formProduct->isSubmitted()){
+            $em=$this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute("listProduct");
+        }
+        return $this->render("product/updateProduct.html.twig",array('formProduct'=>$formProduct->createView()));
+    }
 }
---
+++ src/Form/ProductType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('description')
+            ->add('submit',SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
